8eb7f5affa860ab4355eb4856f84cfe35c463047
Alex Rivard <alex.rivard4406@gmail.com>
2024-10-29 20:03:37 -0500

    small routing adjustments made

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import Container from 'react-bootstrap/Container';
 import { HashRouter as Router, Route, Switch } from 'react-router-dom';
-import Categories from '../Categories/Categories.jsx';
 import Favorites from '../Favorites/Favorites.jsx';
 import Search from '../Search/Search.jsx';
 import NavBar from '../NavBar/NavBar.jsx';
@@ -10,24 +9,20 @@ import NavBar from '../NavBar/NavBar.jsx';
 function App() {
   return (
     <Container>
-    <Router>
-      <header>
-      <h1>Giphy Search!</h1>
-      </header>
-      <NavBar />
-      <Switch>
-      <Route path='/' exact>
-      <Search />
-      </Route>
-      <Route path='/favorites' >
-      <Favorites />
-      </Route>
-
-      {/* <Route path='/catergories' >
-      <Categories />
-      </Route> */}
-      
-        <Route path='/'>
+      <Router>
+        <header>
+          <h1>Giphy Search!</h1>
+        </header>
+        <NavBar />
+        <Switch>
+          <Route path='/' exact>
+            <Search />
+          </Route>
+          <Route path='/favorites' >
+            <Favorites />
+          </Route>
+          
+          <Route path='/'>
             <h2> Page not found - Check URL path</h2>
           </Route>
         </Switch>
